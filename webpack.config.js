76846dbc20a01e900dd82483550e102455801285
seventeen <1253478653@qq.com>
2019-03-21 22:43:27 +0800

    添加webpack的externals

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,23 @@ module.exports = {
     library: 'PUI',
     libraryTarget: 'umd'
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    }
+  },
   module: {
     rules: [
       {
