367160c02a01740c2a0561ae5d15fd4bee449279
seventeen <1253478653@qq.com>
2019-03-30 23:59:28 +0800

    第一版

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,18 +1,1 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target);
-}
-
-ReactDOM.render(<div>
-  <Icon 
-    name="wechat" 
-    onClick={fn} 
-    onMouseEnter={() => console.log('onMouseEnter')}
-    onMouseLeave={() => console.log('onMouseLeave')}  
-  />
-  {/* <Icon name="qq"/>
-  <Icon name="alipay"/> */}
-</div>, document.querySelector('#root'))
+export { default as Icon } from './icon/icon';
