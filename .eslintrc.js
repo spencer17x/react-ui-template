7173371afe284ef981267359cc1f9ae8c994e068
Spencer17x <1253478653@qq.com>
2020-07-17 17:36:10 +0800

    fix: 🐛 修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
 	extends: ['airbnb-typescript'],
 	parserOptions: {
 		project: './tsconfig.json'
+	},
+	plugins: [
+		'react-hooks'
+	],
+	rules: {
+		'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+		'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
 	}
 };
