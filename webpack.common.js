c40c7632a8346bddd7d298e6e1471e031a46892f
Spencer17x <1253478653@qq.com>
2020-07-19 17:09:41 +0800

    fix: 🐛 删除sass-resources-loader

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const { CheckerPlugin } = require('awesome-typescript-loader');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-
 module.exports = {
 	entry: './lib/index.tsx',
 	output: {
@@ -24,12 +23,6 @@ module.exports = {
 					'style-loader', // 将 JS 字符串生成为 style 节点
 					'css-loader', // 将 CSS 转化成 CommonJS 模块
 					'sass-loader', // 将 Sass 编译成 CSS，默认使用 Node Sass
-					{
-						loader: 'sass-resources-loader',
-						options: {
-							resources: './lib/style/index.scss'
-						}
-					}
 				]
 			},
 			{
